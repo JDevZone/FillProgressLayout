8486dbfd412b1ec7dbb100dd39f83654d3af760c
jdevzone <jdevzone@gmail.com>
2019-09-17 11:40:06 +0530

    color setters bug fix

--- fillprogresslayout/src/main/java/com/devzone/fillprogresslayout/FillProgressLayout.kt
+++ fillprogresslayout/src/main/java/com/devzone/fillprogresslayout/FillProgressLayout.kt
@@ -216,12 +216,14 @@ class FillProgressLayout : LinearLayout {
     fun setProgressBackgroundColor(@ColorRes resId: Int) {
         if (isValidRes(resId)) {
             mBackgroundColor = ContextCompat.getColor(context, resId)
+            initPaint()
         }
     }
 
     fun setProgressColor(@ColorRes resId: Int) {
         if (isValidRes(resId)) {
             mProgressColor = ContextCompat.getColor(context, resId)
+            initPaint()
         }
     }
 
