1f3913a44f3e9c03b8b036d37585c2c9b6078654
jdevzone <jdevzone@gmail.com>
2021-05-15 11:15:28 +0530

    #19 bug fixed & versions bumped color int support added

--- app/src/main/java/com/devzone/fpl_sample/MainActivity.kt
+++ app/src/main/java/com/devzone/fpl_sample/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.devzone.fpl_sample
 
+import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.view.View
+import android.view.animation.BounceInterpolator
 import android.view.animation.LinearInterpolator
+import android.view.animation.OvershootInterpolator
 import android.widget.Button
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
@@ -20,7 +23,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         handlePreciseControls()
+
         fillNF.setAnimationInterpolator(LinearInterpolator())
+
+
         fishCardFPL?.apply {
             setProgressUpdateListener { progress ->
                 val color = if (progress > 60) R.color.colorBlue else R.color.colorRed
@@ -30,7 +36,35 @@ class MainActivity : AppCompatActivity() {
                 )
             }
         }
+        fishCardFPL.setAnimationInterpolator(OvershootInterpolator())
+        fishCardFPL.setProgressBackgroundColor(Color.LTGRAY)
+        fishCardFPL.setProgressColor(Color.parseColor("#00ffff"))
 
+    }
+
+    fun toggleFill(view: View) {
+        val isAnimated = animateCB.isChecked
+        val button: Button = view as Button
+        button.isEnabled = false
+        isFilled = !isFilled
+        fillL?.setDoOnProgressEnd { _ ->
+            button.isEnabled = true;button.text = if (isFilled) "UnFill" else "Fill"
+        }
+        fillL?.setProgress(if (isFilled) 100 else 0, isAnimated)
+        fillNF?.setProgress(if (isFilled) 100 else 0)
+        fillL?.setProgressColors(intArrayOf(R.color.colorGradient1, R.color.colorGradient2))
+    }
+
+
+    fun toggleFillLayout(view: View) {
+        if(view !is FillProgressLayout) return
+        val isAnimated = animateCB.isChecked
+        view.apply {
+            when (currentProgress){
+                in 50..99,0 ->  setProgress(100,isAnimated)
+                in 0..50,100 ->  setProgress(0,isAnimated)
+            }
+        }
     }
 
     private fun handlePreciseControls() {
@@ -66,28 +100,8 @@ class MainActivity : AppCompatActivity() {
         fishCardFPL?.setProgress(currentProgress, isAnimated)
     }
 
-    fun toggleFill(view: View) {
-        val isAnimated = animateCB.isChecked
-        val button: Button = view as Button
-        button.isEnabled = false
-        isFilled = !isFilled
-        fillL?.setDoOnProgressEnd { _ ->
-            button.isEnabled = true;button.text = if (isFilled) "UnFill" else "Fill"
-        }
-        fillL?.setProgress(if (isFilled) 100 else 0, isAnimated)
-        fillNF?.setProgress(if (isFilled) 100 else 0)
-        fillL?.setProgressColors(intArrayOf(R.color.colorGradient1, R.color.colorGradient2))
-    }
 
-    fun toggleFillLayout(view: View) {
-        if(view !is FillProgressLayout) return
-        val isAnimated = animateCB.isChecked
-        view.apply {
-            when (currentProgress){
-                in 50..99,0 ->  setProgress(100,isAnimated)
-                in 0..50,100 ->  setProgress(0,isAnimated)
-            }
-        }
-    }
+
+
 
 }
